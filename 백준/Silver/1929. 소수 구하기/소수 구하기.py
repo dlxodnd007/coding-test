d313388d677fa3b8e2aaee80f2a958811e0b6cd4
dlxodnd007 <dlxodnd007@naver.com>
2025-03-12 21:11:42 +0900

    [Silver III] Title: 소수 구하기, Time: 448 ms, Memory: 42788 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,20 @@
+from math import sqrt
+
+M, N = map(int, input().split())
+
+result = 0
+is_prime = [True for _ in range(N + 1)]
+
+is_prime[1] = False
+
+for i in range(2, int(sqrt(N)) + 1):
+    if is_prime[i]:
+        for j in range(2 * i, N + 1, i):
+            is_prime[j] = False
+
+for i in range(1, N + 1):
+    result += is_prime[i]
+
+for i in range(M, N + 1):
+	if is_prime[i]:
+		print(i)
