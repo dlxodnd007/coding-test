1ff3b6820393732c3fef5a94452ff4b0f3de3407
dlxodnd007 <dlxodnd007@naver.com>
2025-03-19 23:57:01 +0900

    [Silver IV] Title: 큐, Time: 64 ms, Memory: 34924 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,29 @@
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+from collections import deque
+
+N = int(input())
+st = deque()
+
+for i in range(N):
+
+	s = input()
+
+	if 'push' in s:
+		x = int(s.split()[1])
+		st.append(x)	
+
+	elif s == 'pop':
+		print(st.popleft() if st else -1)
+
+	elif s == 'size':
+		print(len(st))
+
+	elif s == 'empty':
+		print(1 if not st else 0)
+
+	elif s == 'front':
+		print(st[0] if st else -1)
+
+	elif s == 'back':
+		print(st[-1] if st else -1)
