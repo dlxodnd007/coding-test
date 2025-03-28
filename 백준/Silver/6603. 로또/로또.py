df561a25f8a8063d3aef1c8d7f1bd3ea8a00f8a4
dlxodnd007 <dlxodnd007@naver.com>
2025-03-28 19:59:36 +0900

    [Silver II] Title: 로또, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -1,27 +1,17 @@
-def comb(idx, lev):
-	global choose, arr, k
-
-	if lev == 6:
-		for u in choose:
-			print(u, end =' ')
-		print()
-		return
-
-	for i in range(idx, k):
-		choose.append(arr[i])
-		comb(i + 1, lev + 1)
-		choose.pop()
-
+from itertools import combinations
 
 while True:
-
-	choose = []
 	I = list(map(int, input().split()))
 
 	k = I[0]
 	arr = I[1:]
+
 	if k == 0:
 		break
 
-	comb(0, 0)
+	for comb in combinations(arr, 6):
+		for u in comb:
+			print(u , end =' ')
+		print()
+
 	print()
