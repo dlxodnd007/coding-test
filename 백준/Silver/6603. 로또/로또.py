c377b4059f2c878f39e127ec75e54ed7d1b679ba
dlxodnd007 <dlxodnd007@naver.com>
2025-03-28 19:59:11 +0900

    [Silver II] Title: 로또, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.py
@@ -0,0 +1,27 @@
+def comb(idx, lev):
+	global choose, arr, k
+
+	if lev == 6:
+		for u in choose:
+			print(u, end =' ')
+		print()
+		return
+
+	for i in range(idx, k):
+		choose.append(arr[i])
+		comb(i + 1, lev + 1)
+		choose.pop()
+
+
+while True:
+
+	choose = []
+	I = list(map(int, input().split()))
+
+	k = I[0]
+	arr = I[1:]
+	if k == 0:
+		break
+
+	comb(0, 0)
+	print()
