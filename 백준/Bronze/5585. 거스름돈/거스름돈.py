e432e1ca75d082f89b49e796ae1fd2e2dfbbf13d
dlxodnd007 <dlxodnd007@naver.com>
2025-04-05 19:50:35 +0900

    [Bronze II] Title: 거스름돈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.py
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -1,14 +1,9 @@
 money = 1000 - int(input())
-ans = int(1e12)
+coins = [500, 100, 50, 10 ,5, 1]
+cnt = 0
 
-for c500 in range(2):
-	for c100 in range(10):
-		for c50 in range(20):
-			for c10 in range(100):
-				for c5 in range(200):
-					value = (500 * c500) + (100 * c100) + (50 * c50) + (10 * c10) + (5 * c5)
+for coin in coins:
+	cnt += money // coin
+	money %= coin
 
-					if money - value >= 0:
-						ans = min(ans, c500 + c100 + c50 + c10 + c5 + (money - value))
-
-print(ans)
+print(cnt)
