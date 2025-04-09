7e1a5a8c33a378f14663f4d5454a519e05959ef0
dlxodnd007 <dlxodnd007@naver.com>
2025-04-09 21:22:22 +0900

    [Gold V] Title: 평범한 배낭, Time: 1568 ms, Memory: 141288 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,3 +1,18 @@
+def func(n, k):
+	global W, V, dp
+
+	if n == 0 or k == 0:
+		return 0
+
+	if dp[n][k] != -1:
+		return dp[n][k]
+
+	dp[n][k] = func(n - 1, k)
+	if k - W[n] >= 0:
+		dp[n][k] = max(dp[n][k] , func(n - 1, k - W[n]) + V[n])
+
+	return dp[n][k]
+
 N, K = map(int, input().split())
 
 W = [0]
@@ -8,12 +23,6 @@ for _ in range(N):
 	W.append(w)
 	V.append(v)
 
-dp = [[0 for _ in range(K + 1)] for _ in range(N  + 1)]
+dp = [[-1 for _ in range(K + 1)] for _ in range(N  + 1)]
 
-for n in range(1, N + 1):
-	for k in range(1, K  + 1):
-		dp[n][k] = dp[n - 1][k]
-		if k - W[n] >= 0:
-			dp[n][k] = max(dp[n][k], dp[n - 1][k - W[n]] + V[n])
-
-print(dp[N][K])
+print(func(N, K))
