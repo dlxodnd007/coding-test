6c3e2c19c58ed402b3bc298d401c36012ee6ef0b
dlxodnd007 <dlxodnd007@naver.com>
2025-04-10 19:04:12 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 424 ms, Memory: 41352 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -1,23 +1,27 @@
 import sys
 sys.setrecursionlimit(10**6)
+from collections import deque
 
-def dfs(y, x):
+def bfs(y, x):
 	global N, M, matrix, visited, dy, dx, ans
 
-	if visited[y][x]:
-		return
-
-	if matrix[y][x] == "P":
-		ans += 1
-
+	q = deque()
+	q.append((y, x))
 	visited[y][x] = True
 
-	for i in range(4):
-		ny = dy[i] + y
-		nx = dx[i] + x
-		
-		if (0 <= ny < N) and (0 <= nx < M) and (matrix[ny][nx] != 'X') and (not visited[ny][nx]):
-			dfs(ny, nx)
+	while q:
+		y, x = q.popleft()
+
+		if matrix[y][x] == "P":
+			ans += 1
+
+		for i in range(4):
+			ny = dy[i] + y
+			nx = dx[i] + x
+			if (0 <= ny < N) and (0 <= nx < M) and (matrix[ny][nx] != 'X') and (not visited[ny][nx]):
+				q.append((ny, nx))
+				visited[ny][nx] = True
+
 
 N , M = map(int, input().split())
 matrix = [list(input()) for _ in range(N)]
@@ -28,14 +32,9 @@ dy = [-1, 0, 1, 0]
 dx = [0, 1, 0, -1]
 
 
-found = False
 for y in range(N):
 	for x in range(M):
 		if matrix[y][x] == "I":
-			dfs(y, x)
-			found = True
+			bfs(y, x)
 			break
-	if found:
-		break
-
 print(ans if ans != 0 else "TT")
