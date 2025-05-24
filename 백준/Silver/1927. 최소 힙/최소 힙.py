57a9976bab0b405294fb38472114b8abf1a84f6b
dlxodnd007 <dlxodnd007@naver.com>
2025-05-24 11:02:10 +0900

    [Silver II] Title: 최소 힙, Time: 116 ms, Memory: 38340 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,19 @@
+import heapq
+import sys	
+input = lambda : sys.stdin.readline().rstrip()
+
+heap = []
+N = int(input())
+
+for _ in range(N):
+
+	x = int(input())
+	
+	if x == 0:
+		if heap:
+			print(heapq.heappop(heap))
+		else:
+			print(0)
+
+	else:
+		heapq.heappush(heap, x)
