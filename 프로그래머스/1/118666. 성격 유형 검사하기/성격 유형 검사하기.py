d8774576390c12959fa418192e630374b4f5ead2
dlxodnd007 <dlxodnd007@naver.com>
2025-06-05 17:31:59 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.26 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -0,0 +1,23 @@
+types = ['R', 'T', 'C', 'F', 'J', 'M', 'A', 'N']
+
+def solution(survey, choices):
+    score = {t: 0 for t in types} # score[t]: 성격 유형
+
+    # 점수 계산
+    for s, c in zip(survey, choices):
+        if c <= 3:
+            score[s[0]] += (4 -c)
+        elif c >= 5:
+            score[s[1]] += (c -4)
+            
+    # 답 구하기
+    ans = ''
+    for i in range(4):
+        s1, s2 = score[types[2 * i]], score[types[2 * i + 1]]
+        if s1 >= s2:
+            ans += types[2 * i]
+        else:
+            ans += types[2 * i + 1]
+    
+    
+    return ans
