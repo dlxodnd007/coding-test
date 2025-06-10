0ebd6a3043613d7a76c432901e972fd48e4ce2c8
dlxodnd007 <dlxodnd007@naver.com>
2025-06-10 21:50:14 +0900

    [Silver II] Title: 마인크래프트, Time: 736 ms, Memory: 113040 KB -BaekjoonHub

---
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -0,0 +1,26 @@
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+N, M, B = map(int, input().split())
+field = [list(map(int, input().split())) for _ in range(N)]
+answer = sys.maxsize
+idx = 0
+
+for floor in range(257):
+    exceed_block, lack_block = 0, 0 
+
+    for i in range(N):
+        for j in range(M):
+
+            if field[i][j] > floor : 
+                exceed_block += field[i][j] - floor
+            else : 
+                lack_block += floor - field[i][j]
+
+    if exceed_block + B >= lack_block :
+        if (exceed_block * 2) + lack_block <= answer:
+            answer = (exceed_block * 2) + lack_block
+            idx = floor
+
+
+print(answer, idx)
